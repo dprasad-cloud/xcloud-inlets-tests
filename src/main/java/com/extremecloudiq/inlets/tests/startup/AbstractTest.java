85af1d242fd9404a3b76b67140a5a6a3bee8e4c7
Lakshmi Prasad Durgi <dprasad@extremenetworks.com>
2023-07-11 15:23:16 -0700

    updates stats to redis, make test framework

---
+++ src/main/java/com/extremecloudiq/inlets/tests/startup/AbstractTest.java
@@ -0,0 +1,165 @@
+package com.extremecloudiq.inlets.tests.startup;
+
+import lombok.Data;
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+@Data
+@Configuration
+public abstract class AbstractTest {
+
+    private static final String INLETS_TESTS_PREFIX = "inlets-tests:";
+    private boolean enabled;
+    private int poolSize;
+    private int maxDevices;
+    private long waitTime;
+
+    @Value("${inlets.tests.sleep.time:1000}")
+    private long sleepTime;
+
+    @Value("${inlets.tests.serial.realdevices:2021Q-30238}")
+    private String realDevices;
+
+    @Value("${inlets.tests.serial.prefix:IQEMU}")
+    private String serialPrefix;
+
+    @Value("${inlets.tests.stats.interval:600000}")
+    private long statsInterval;
+
+    @Autowired
+    RedisTemplate<String, String> redisTemplate;
+
+    static Map<String,String> shortNameMap = new HashMap<>();
+    static{
+        shortNameMap.put(DeviceConnectHealthCheckTest.class.getSimpleName(),"DHC");
+        shortNameMap.put(NOSSOverInletsTest.class.getSimpleName(),"COI");
+        shortNameMap.put(NOSSVersionOverInletsTest.class.getSimpleName(),"VOI");
+        shortNameMap.put(SshOverInletsTest.class.getSimpleName(),"SSH");
+
+    }
+    public void setValuesAndStartTests(org.slf4j.Logger logger, boolean enabled, int poolSize, int maxDevices, long waitTime) {
+        this.enabled = enabled;
+        this.poolSize = poolSize;
+        this.maxDevices = maxDevices;
+        this.waitTime = waitTime;
+
+        if (!enabled) {
+            logger.info("Tests not enabled.");
+            return;
+        }
+
+        new Thread() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                while (true) {
+                    long t1 = System.currentTimeMillis();
+                    ExecutorService service = Executors.newFixedThreadPool(poolSize);
+                    for (int j = 0; j < poolSize; j = j + 1) {
+                        service.submit(new Runner(j, poolSize));
+                    }
+
+                    System.out.println("Tasks submitted: time taken = " + (System.currentTimeMillis() - t1));
+                    service.shutdown();
+                    service.awaitTermination(waitTime, TimeUnit.MILLISECONDS);
+                    System.out.println("completed current iteration, time taken = " + (System.currentTimeMillis() - t1));
+
+                    Thread.sleep(sleepTime);
+
+                }
+
+            }
+        }.start();
+
+    }
+
+    public void runOverDevices(int start) {
+        int startIndex = (maxDevices / poolSize) * start;
+        int lastIndex = (startIndex + maxDevices / poolSize);
+
+        //Handle realDevices
+        //Only execute in the first thread
+        if (start == 0) {
+            List<String> realDevicesList = Arrays.stream(realDevices.split(",")).
+                filter(s -> s.length() > 0).
+                collect(Collectors.toList());
+            for (String serialNumber : realDevicesList)
+                execute(serialNumber);
+
+        }
+
+        for (int i = startIndex; i < lastIndex; i++) {
+
+            String serialNumber = String.format(serialPrefix, i);
+            execute(serialNumber);
+
+        }
+    }
+
+    class Runner extends Thread {
+        int start;
+        int poolSize;
+
+        public Runner(int start, int max) {
+            this.start = start;
+            this.poolSize = max;
+        }
+
+        public void run() {
+            runOverDevices(start);
+        }
+
+    }
+
+    static long lastStatsUpdate = System.currentTimeMillis();
+    static Map<String, Map<String, Long>> respMap = new HashMap<>();
+    static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyMMddHHmmss");
+
+    @SneakyThrows
+    public void logResponse(Logger logger, String resp, long st, String serialNumber) {
+        resp = resp != null ? resp.replaceAll(" ", "") : resp;
+        long respTime = System.currentTimeMillis() - st;
+        logger.debug("resp: " + resp);
+        logger.info(serialNumber + "=" + resp + " | " + respTime);
+
+        //Update stats (after every N records) to Redis for ease of access
+        lastStatsUpdate++;
+
+        String className = shortNameMap.get(Class.forName((logger.getName())).getSimpleName());
+        Map<String, Long> map = respMap.getOrDefault(className, new HashMap<>());
+        map.put(resp, map.getOrDefault(resp, 0L) + 1);
+        map.put("TOT", map.getOrDefault("TOT", 0L) + 1);
+        map.put("TRT", map.getOrDefault("TRT", 0L) + respTime);
+        map.put("ART", map.get("TRT") / map.get("TOT"));
+        respMap.put(className, map);
+
+        if ((System.currentTimeMillis() - lastStatsUpdate) >= statsInterval) {
+            synchronized (respMap) {
+                try {
+                    String mapAsString = respMap.toString();
+                    redisTemplate.opsForValue().set(INLETS_TESTS_PREFIX + simpleDateFormat.format(new Date()), mapAsString);
+                    System.out.println("-------------------------->" + respMap);
+                    lastStatsUpdate = System.currentTimeMillis();
+                    respMap.clear();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    logger.error("Error updating stats...", e);
+                }
+            }
+        }
+    }
+
+    public abstract void execute(String serialNumber);
+}
--- src/main/java/com/extremecloudiq/inlets/tests/startup/DeviceConnectHealthCheckTest.java
+++ src/main/java/com/extremecloudiq/inlets/tests/startup/DeviceConnectHealthCheckTest.java
@@ -1,7 +1,6 @@
 package com.extremecloudiq.inlets.tests.startup;
 
 import com.extremecloudiq.inlets.tests.vo.DeviceConnectBaseRequest;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -9,25 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 /**
  * @author dprasad
  */
 @Slf4j
 @Configuration
-public class DeviceConnectHealthCheckTest {
-
-    @Value("${inlets.tests.serial.prefix:IQEMU}")
-    private String serialPrefix;
-
-    @Value("${inlets.tests.serial.realdevices:2021Q-30238}")
-    private String realDevices;
+public class DeviceConnectHealthCheckTest extends AbstractTest {
 
     @Value("${inlets.tests.hc.enable:true}")
     private boolean enabled;
@@ -44,99 +31,31 @@ public class DeviceConnectHealthCheckTest {
     @Value("${inlets.tests.hc.wait.time:180000}")
     private long waitTime;
 
-    @Value("${inlets.tests.sleep.time:1000}")
-    private long sleepTime;
-
     RestTemplate restTemplate = new RestTemplate();
 
-    class Runner extends Thread {
-        int start = 0;
-        int poolSize = 0;
+    public void execute(String serialNumber) {
+        DeviceConnectBaseRequest request = new DeviceConnectBaseRequest();
+        request.setDeviceFamily("VSP_SERIES");
+        request.setDeviceModel("VOSS5520");
+        request.setInletsVersion("0.1.0.0");
+        request.setPlatform("VOSS5520");
+        long st = 0L;
+        String resp;
+        try {
+            st = System.currentTimeMillis();
+            ResponseEntity<String> response = restTemplate.postForEntity(healthCheckBaseUrl + serialNumber, request, String.class);
+            resp = response.getStatusCode() + "";
 
-        public Runner(int start, int max) {
-            this.start = start;
-            this.poolSize = max;
+        } catch (Exception e) {
+            resp = e.getMessage();
         }
 
-        public void run() {
+        logResponse(log, resp, st, serialNumber);
 
-            int startIndex = (maxDevices / poolSize) * start;
-            int lastIndex = (startIndex + maxDevices / poolSize);
-
-            //Handle realDevices
-            //Only execute in the first thread
-            if(start == 0){
-                List<String> realDevicesList = Arrays.stream(realDevices.split(",")).
-                    filter(s -> s.length() >0).
-                    collect(Collectors.toList());
-                for(String serialNumber: realDevicesList)
-                    execute(serialNumber);
-
-            }
-
-            for (int i = startIndex; i < lastIndex; i++) {
-
-                String serialNumber = String.format(serialPrefix, i);
-                execute(serialNumber);
-
-            }
-        }
-
-        private void execute(String serialNumber) {
-            DeviceConnectBaseRequest request = new DeviceConnectBaseRequest();
-            request.setDeviceFamily("VSP_SERIES");
-            request.setDeviceModel("VOSS5520");
-            request.setInletsVersion("0.1.0.0");
-            request.setPlatform("VOSS5520");
-            long st = 0L;
-            String resp = null;
-            try {
-                st = System.currentTimeMillis();
-                ResponseEntity<String> response = restTemplate.postForEntity(healthCheckBaseUrl + serialNumber, request, String.class);
-                resp = response.getStatusCode()+"";
-
-            } catch (Exception e) {
-                resp = e.getMessage();
-            }
-
-            resp = resp!=null?resp.replaceAll(" ", ""):resp;
-            long et = System.currentTimeMillis();
-            log.debug("resp: " + resp);
-            log.info(serialNumber +"="+ resp + " | " + (et - st));
-
-        }
     }
 
     @PostConstruct
     public void init() {
-
-        if (!enabled) {
-            log.info("Health Check tests not enabled.");
-            return;
-        }
-
-        new Thread() {
-            @SneakyThrows
-            @Override
-            public void run() {
-                while (true) {
-                    long t1 = System.currentTimeMillis();
-                    ExecutorService service = Executors.newFixedThreadPool(poolSize);
-                    for (int j = 0; j < poolSize; j = j + 1) {
-                        service.submit(new Runner(j, poolSize));
-                    }
-
-                    System.out.println("Tasks submitted: time taken = " + (System.currentTimeMillis() - t1));
-                    service.shutdown();
-                    service.awaitTermination(waitTime, TimeUnit.MILLISECONDS);
-                    System.out.println("completed current iteration, time taken = " + (System.currentTimeMillis() - t1));
-
-                    Thread.sleep(sleepTime);
-
-                }
-
-            }
-        }.start();
-
+        setValuesAndStartTests(log, enabled, poolSize, maxDevices, waitTime);
     }
 }
